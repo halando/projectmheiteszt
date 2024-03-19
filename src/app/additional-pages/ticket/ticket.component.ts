be85a99ad8d6021a2e14e0409805928d6201ecdd
andoattila698@gmail.com <andoattila698@gmail.com>
2024-03-20 00:43:04 +0100

    Események,Jegyek

--- src/app/additional-pages/ticket/ticket.component.ts
+++ src/app/additional-pages/ticket/ticket.component.ts
@@ -1,4 +1,21 @@
 import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+
+export interface Element {
+  name: string;
+  position: number;
+  discount: string;
+  price: number;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {position: 1, name: 'Felnőtt', discount: 'Nincs', price: 3600 },
+  {position: 2, name: 'Diák', discount: 'Van', price: 2400},
+  {position: 3, name: 'Nyugdíjas', discount: 'Van', price: 2400},
+  {position: 4, name: 'gyerek', discount: 'Ingyenes', price: 0},
+  
+];
+
 
 @Component({
   selector: 'app-ticket',
@@ -6,5 +23,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./ticket.component.css']
 })
 export class TicketComponent {
-
+  displayedColumns: string[] = ['demo-position', 'demo-name', 'demo-discount','demo-price'];
+  dataSource = ELEMENT_DATA;
 }
